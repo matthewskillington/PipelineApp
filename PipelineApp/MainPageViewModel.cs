582af7b360f9e9aa1e8d0fabfa08a686a70684fc
matthewskillington <matthew.skillington@ideagenplc.com>
2020-06-29 14:57:43 +0100

    First commit

---
+++ PipelineApp/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PipelineApp
+{
+    public class MainPageViewModel : BindableObject
+    {
+        private string _buttonText;
+        private string _entryText;
+
+        public ICommand ButtonClickedCommand { get; private set; }
+
+        public MainPageViewModel(string buttonText)
+        {
+            ButtonText = buttonText;
+            ButtonClickedCommand = new Command(HandleButtonClicked);
+            EntryText = "Enter some text to change the button";
+
+        }
+
+        private void HandleButtonClicked()
+        {
+            ButtonText = EntryText;
+        }
+
+        public string EntryText
+        {
+            get => _entryText;
+            set
+            {
+                if (value == _entryText) return;
+                _entryText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ButtonText
+        {
+            get => _buttonText;
+            set
+            {
+                if (value == _buttonText) return;
+                _buttonText = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ PipelinesApp.UnitTests/MainPageViewModelTests.cs
@@ -0,0 +1,33 @@
+using System;
+using PipelineApp;
+using Xunit;
+
+namespace PipelinesApp.UnitTests
+{
+    public class MainPageViewModelTests
+    {
+        [Fact]
+        public void EntryTextString_InitializesProperly()
+        {
+            //Arrange:
+            var vm = new MainPageViewModel("Click me");
+
+            //Assert:
+            Assert.Equal(vm.EntryText, "Enter some text to change the button");
+        }
+
+        [Fact]
+        public void ButtonText_OnPress_ChangesToInputText()
+        {
+            //Arrange:
+            var vm = new MainPageViewModel("Click me");
+
+            //Act:
+            vm.EntryText = "Some new text";
+            vm.ButtonClickedCommand.Execute(null);
+
+            Assert.Equal(vm.ButtonText, "Some new text");
+            Assert.Equal(vm.ButtonText, vm.EntryText);
+        }
+    }
+}
